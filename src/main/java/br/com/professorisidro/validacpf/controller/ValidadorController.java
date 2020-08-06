520cc7349d9cd0c10c5a765eef6e5cf08a17a2de
flaviacardoso <flaviaferreira.cardo@gmail.com>
2020-08-06 14:04:19 -0300

    criando rota home

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -19,4 +19,10 @@ public class ValidadorController {
 		 return "{\"status\": \"invalido\"}";
 	}
 
+	@GetMapping("/")
+	public String index() {
+		
+		 return "{\"mensagem\": \"Bem vindo a API turma 6 Itau\"}";
+	}
+
 }
